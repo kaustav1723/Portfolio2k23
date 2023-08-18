cd738227bd6722a722726b844496be8fa1bc57ec
KaustavSarkar <kaustavsarkar0539@gmail.com>
2023-08-19 01:14:29 +0530

    set base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Columns } from 'lucide-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base :"/Portfolio2k23"
 })
